838d8399bc9798424e6c73c0f7dfa56a4aa9b2a6
miyakawa <haru19970409@gmail.com>
2021-01-24 16:58:09 +0900

    Google auth client更新

--- exdrawapp/lib/PaintPage.dart
+++ exdrawapp/lib/PaintPage.dart
@@ -192,9 +192,9 @@ class GoogleHttpClient extends IOClient {
 
   GoogleHttpClient(this._headers) : super();
 
-  // @override
-  // Future<http.StreamedResponse> send(http.BaseRequest request) =>
-  //     super.send(request..headers.addAll(_headers));
+  @override
+  Future<IOStreamedResponse> send(http.BaseRequest request) =>
+      super.send(request..headers.addAll(_headers));
 
   @override
   Future<http.Response> head(Object url, {Map<String, String> headers}) =>
